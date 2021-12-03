887399c9839129628a3f2bd9e2da7c761e7d3483
Yualbe Luis <lois21us@gmail.com>
2021-12-03 16:38:41 +0100

    Completando la instalacion de github

---
+++ database/migrations/2021_12_03_103707_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('image');
+            $table->text('description');
+            $table->timestamps();
+            $table->foreign('user_id')->reference('id')->on('users');
+            $table->foreign('category_id')->reference('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
