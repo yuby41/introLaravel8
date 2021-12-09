3af676c040818074f9f5305499054e64dc8328c5
Yualbe Luis <lois21us@gmail.com>
2021-12-09 17:20:54 +0100

    Listado de lecciones

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -7,7 +7,16 @@
                 <li class="font-medium text-sm text-gray-400 uppercase mb-4">
                     Contenido
                 </li>
-                ---
+                @foreach($course->posts as $post)
+                <li class="flex items-center text-gray-600 mt-2">
+                    {{ $post->name }}
+                    @if($post->free)
+                    <span 
+                        class="text-xs text-gray-500 font-semibold bg-gray-300 px-2 rounded-full ml-auto"
+                    >Gratis</span>
+                    @endif
+                </li>
+                @endforeach
             </ul>
         </div>
         <div class="text-gray-700 col-span-2">
